aa1c5c56080aee0977b1a07cbdb4c5aa4f24e3d5
Hacker00002 <abdulbositsharipovv@gmail.com>
2022-10-17 19:07:59 +0500

    j

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+//dars-1
+
 // let maxSpeed = 15
 
 // maxSpeed = 450
@@ -5,8 +7,68 @@
 
 // console.log(maxSpeed)
 
-let abu = 28
+//let abu = 28
 
-abu = 3
+//abu = 3
 
-console.log (abu)
+//console.log (abu)
+
+//dars-finish-1
+
+
+
+//dars-2
+let person = {
+    name: 'Abdulbosit',
+    surname: 'Sharipov',
+    age: 17,
+    isMarried: false,
+    isStudying: true
+}
+
+console.log(person)
+
+let array = ['school', 'name', 'surname']
+
+console.log(array)
+
+array[0] = 'Car'
+
+console.log(array)
+
+let x = 4
+let y = 9
+
+console.log(x+y)
+
+let c = 15
+console.log(c % 9)
+
+y++
+
+console.log(y)
+
+x--
+
+console.log(x)
+
+let a = 90
+let s = 90
+
+console.log(a == s)
+
+console.log('90' == 90)
+
+console.log(9/0)
+
+console.log('89' + 89)
+
+alert('salom dunyo')
+
+let savol = confirm('siz voyaga yetkanmisiz')
+
+console.log(savol)
+
+let answer = prompt('bizning wen saytimiz yoqdimi')
+
+console.log(answer)
